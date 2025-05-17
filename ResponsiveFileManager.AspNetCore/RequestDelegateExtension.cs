73d91e520b23546ecdf7cd4c691a0b72cc575129
Jakub Míšek <jmisek@gmail.com>
2025-05-17 17:15:18 +0200

    correct working directory
    - fixed resolution of relative paths to be consistent across the app
    - PHP app root ~ PHP app working directory

--- ResponsiveFileManager.AspNetCore/RequestDelegateExtension.cs
+++ ResponsiveFileManager.AspNetCore/RequestDelegateExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Reflection;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
@@ -14,30 +15,56 @@ namespace Microsoft.AspNetCore.Builder;
 /// </summary>
 public static class RequestDelegateExtension
 {
+    const string filemanagerPath = "filemanager";
+
+    static string FileManagerPath()
+    {
+        var defaultPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), filemanagerPath);
+
+        foreach (var path in new string[] {
+            defaultPath,
+            Path.Combine(Directory.GetCurrentDirectory(), filemanagerPath),
+        })
+        {
+            if (Directory.Exists(path))
+            {
+                return Path.GetFullPath(path);
+            }
+        }
+
+        // default
+        return defaultPath;
+    }
+
     /// <summary>
     /// Installs ResponsiveFileManager middleware.
     /// </summary>
     /// <param name="app">The application builder.</param>
     public static IApplicationBuilder UseResponsiveFileManager(this IApplicationBuilder app)
     {
-        const string filemanagerPath = "/filemanager";
+        var root = Path.GetDirectoryName(FileManagerPath()); // parent of 'filemanager' is root of the app
+        var requestPath = new PathString($"/{filemanagerPath}");
 
         app.UseStaticFiles(new StaticFileOptions
         {
-            RequestPath = filemanagerPath,
-            FileProvider = new PhysicalFileProvider(Path.GetFullPath(Path.Combine(Assembly.GetEntryAssembly().Location, ".." + filemanagerPath))),
+            RequestPath = requestPath,
+            FileProvider = new PhysicalFileProvider(Path.Combine(root, filemanagerPath)),
         });
 
-        app.UsePhp(filemanagerPath, (Context ctx) =>
-        {
-            // construct the options
-            var options = new ResponsiveFileManagerOptions();
-            ctx.GetService<IConfiguration>().GetSection("ResponsiveFileManagerOptions").Bind(options);
-            ctx.GetService<IConfigureOptions<ResponsiveFileManagerOptions>>()?.Configure(options);
+        app.UsePhp(
+            requestPath, // filter by prefix
+            ctx =>
+            {
+                // construct the options
+                var options = new ResponsiveFileManagerOptions();
+                ctx.GetService<IConfiguration>().GetSection(nameof(ResponsiveFileManagerOptions)).Bind(options);
+                ctx.GetService<IConfigureOptions<ResponsiveFileManagerOptions>>()?.Configure(options);
 
-            // pass the options object to PHP globals
-            ctx.Globals["rfm_options"] = PhpValue.FromClass(options);
-        });
+                // pass the options object to PHP globals
+                ctx.Globals["rfm_options"] = PhpValue.FromClass(options);
+            },
+            root // root of the app
+        );
 
         return app;
     }
